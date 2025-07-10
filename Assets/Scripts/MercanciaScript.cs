8bc2b21a886d6816eb4fea3b7a668cf29fcb4c87
juligric <49377944@est.ort.edu.ar>
2025-07-09 23:02:29 -0300

    TUVE QUE PASARLO A OTRO REPOSITORIO

---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public float cost;
+    public MoneyManager moneyManager;
+
+     void Start()
+    {
+        if (moneyManager == null)
+        {
+            moneyManager = FindObjectOfType<MoneyManager>();
+        }
+      
+    }
+
+    public void Comprar()
+    {
+        if (moneyManager != null)
+        {
+            if (moneyManager.money >= cost)
+            {
+                moneyManager.UpdateMoney(-cost);
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("No tenes plata :(");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("MoneyManager no encontrado");
+        }
+    }
+}
---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+    public UIManager uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(uiManager == null)
+        {
+            uiManager.UpdateMoneyTxt(money.ToString());
+            if(uiManager == null)
+            {
+                Debug.LogWarning("UIManager no asignado en Money Manager y no se encontro en la escena.");
+            }
+        }
+        uiManager?.UpdateMoneyTxt(money.ToString());
+    }
+
+    public void UpdateMoney(float moneyToAdd)
+    {
+
+        if (money + moneyToAdd < 0)
+        {
+            Debug.Log("No te alcanza");
+            return;
+        }
+        money += moneyToAdd;
+        if(uiManager == null)
+        {
+            uiManager = FindObjectOfType<UIManager>();
+        }
+        uiManager.UpdateMoneyTxt(money.ToString());
+    }
+}
